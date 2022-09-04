79b9cf0781b179c2bc0f2fe875a1ae7685924cb6
Sergey-Yurtaev <engineerk2@mail.ru>
2022-09-04 22:00:40 +0500

    Initial Commit

---
+++ SolarSystem_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolarSystem_MVVM
+//
+//  Created by  Sergey Yurtaev on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
